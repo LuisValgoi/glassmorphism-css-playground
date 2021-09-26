585509a6454bbcce4b54518572431b548b2661f3
Luis Valgoi <luisvalgoi@gmail.com>
2021-09-26 17:38:23 -0300

    feat: added project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const Home: NextPage = () => {
   return (
@@ -13,60 +16,34 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.shapeBlue}></div>
+        <div className={styles.shapeOrange}></div>
+        <form className={styles.form}>
+          <h3>Login Here</h3>
+          <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Email or Phone" id="username" />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <label htmlFor="password">Password</label>
+          <input type="password" placeholder="Password" id="password" />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <button>Log In</button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <div className={styles.social}>
+            <div className="go">
+              <FontAwesomeIcon icon={faGoogle} /> Google
+            </div>
+            <div className={styles.fb}>
+              <FontAwesomeIcon icon={faFacebook} /> Facebook
+            </div>
+          </div>
+        </form>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p>Powered by Luis Valgoi 😋</p>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
